bce4282393d18f9581bf7f585eeb393b41614e4d
Bimal Shrestha <69587963+bimalsh01@users.noreply.github.com>
2023-08-27 13:50:38 +0545

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:emart/screens/SplashScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const SplashScreen(),
+    },
+  ));
+}
